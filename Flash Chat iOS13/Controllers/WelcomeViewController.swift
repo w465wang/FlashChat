57be1a34fec022f2036a83382d6452a0eb2142e6
w465wang <w465wang@uwaterloo.ca>
2020-01-20 14:47:41 -0500

    initial commit

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,38 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
+        super.viewDidLoad() // always call super when overriding function
+        
+        titleLabel.text = K.appName
+        
+//        titleLabel.text = ""
+//        let titleText = "⚡️FlashChat"
+//        for charIndex in 0..<titleText.count {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
+//                self.titleLabel.text?.append(Array(titleText)[charIndex])
+//            }
+//        }
     }
     
 
 }
+
